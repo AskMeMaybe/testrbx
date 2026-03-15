2e5deae843b0bceec038f13a7b1cb73c5b22fe0d
AskMeMaybe <rizkic0d3@gmail.com>
2026-03-16 01:09:31 +0700

    Create loader.lua for Fish It Webhook UI
    Add loader script for Fish It Webhook UI.

---
+++ loader.lua
@@ -0,0 +1,7 @@
+-- ============================================================
+-- Fish It Webhook + UI  — LOADER
+-- Execute file INI SAJA di Delta Executor
+-- Ganti URL di bawah dengan raw URL dari GitHub Anda
+-- ============================================================
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AskMeMaybe/testrbx/refs/heads/main/fish.lua", true))()
